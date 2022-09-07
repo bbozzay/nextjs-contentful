a55b75ed2930525e71046b89c8740bc554b99407
Ben Bozzay <ben@fullstackdigital.com>
2022-09-07 17:05:12 -0600

    feat: logo section

---
+++ components/logos.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx';
+
+type LogoImage = {
+	src: string;
+	height: "7"|"8"|"9"|"10"|"11"|"12";
+	alt: string;
+}
+// Require 4 logos
+export type LogoImages = [LogoImage, LogoImage, LogoImage, LogoImage];
+export type LogoProps = {
+	images: LogoImages;
+}
+const Logos = (props: LogoProps) => {
+	const { images } = props;
+	return (
+		<section className="bg-white">
+			<div className={clsx({"max-w-4xl": images.length === 4}, "mx-auto py-12 px-4 sm:px-6 lg:px-8")}>
+				<div className={clsx({"md:grid-cols-2 lg:grid-cols-4": images.length === 4}, "grid grid-cols-2 gap-12")}>
+					{images.map(({ src, height, alt}) => {
+						return (
+							<div key={src} className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+								<img className={clsx(`h-${height??'12'}`)}  src={src} alt={alt} />
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export { Logos }
