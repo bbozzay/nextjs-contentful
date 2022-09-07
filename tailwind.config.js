6af21bc8820826aee88a97fedfd3dcb14a54001b
Ben Bozzay <ben@fullstackdigital.com>
2022-09-07 17:07:27 -0600

    refactor: update tailwind config to more closely match mockup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      primary: "#2A206A",
+      secondary: "#1CC283"
+    },
+    extend: {
+      fontSize: {
+        '4xl': ['40px', '48px'],
+        '5xl': ['50px', '55px']
+      }
+    },
   },
   plugins: [],
 }
